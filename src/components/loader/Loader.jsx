44b692acea4bdf6d7fe0be160e11aa746de4a249
Elochukwu3 <oguajuromauld5@gmail.com>
2023-01-13 13:39:48 +0100

    init: first commit

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loader.css'
+
+const Loader = () => {
+  return (
+   <div className='container  h-screen grid grid-rows-1 place-items-center bg-white items-center'>
+     <span class="loader"></span>
+   </div>
+  )
+}
+
+export default Loader
