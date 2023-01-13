59f474171b4f002f9bcf9509ac160d2d3b0867ec
Elochukwu3 <oguajuromauld5@gmail.com>
2023-01-13 19:53:56 +0100

    feat: add carousel images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useEffect, useState } from "react";
 import Loader from "./components/loader/Loader";
 
-
 function App() {
-  return (
-    <div className="App">
-    <Loader/>
-    </div>
-  );
+  const [spinner, setSpinner] = useState(true);
+
+  useEffect(() => {
+   const timeOut = setTimeout(() => setSpinner(false), 3500);
+   return () => timeOut
+  }, []);
+  
+  
+
+  return spinner ? <Loader /> : <div className="App">hello</div>;
 }
 
 export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,9 @@
+import slide_image_one from "../images/slid-img.png";
+import slide_image_two from "../images/slid-img.png";
+import slide_image_three from "../images/slid-img.png";
+
+export const slider_data = [
+    {image: slide_image_one, caption: ""},
+    {image: slide_image_two, caption: ""},
+    {image: slide_image_three, caption: ""},
+]
