b280baa25fc3f0d8e3b6798e3cd13b57d7e5429b
collins <kevin1520smart@gmail.com>
2023-01-15 22:17:16 +0100

    starting up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import Modal from "./components/Modal"
+import Progress from "./components/Progress"
 
 function App() {
   
   return (
     <div className="App">
-
+      <Progress/>
+      <Modal
+        visible={true}
+        desc="Sucessful!"
+        txt="Please wait a moment, we are preparing for you..."
+      />
     </div>
   );
 }
