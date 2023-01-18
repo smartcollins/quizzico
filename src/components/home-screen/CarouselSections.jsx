cdcacb2dbb135dcef81dad406c4451ecf00b5399
Elochukwu3 <oguajuromauld5@gmail.com>
2023-01-19 00:37:25 +0100

    feat: add top collection carousel

--- src/components/home-screen/CarouselSections.jsx
+++ src/components/home-screen/CarouselSections.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CarouselSections = ({ image, miniImg, headerOne, headerTwo, title }) => {
   return (
     <div className="space-y-4 rounded-3xl overflow-hidden w-30 shadow-xl mx-3">
-      <div className="h-40 w-full bg-black">
+      <div className="h-40 w-full overflow-hidden">
         <img src={image} alt="" className="h-full w-full" />
       </div>
       <div className="px-2 space-y-1">
@@ -15,7 +15,7 @@ const CarouselSections = ({ image, miniImg, headerOne, headerTwo, title }) => {
         </h2>
       </div>
       <div className="flex gap-3 capitalize items-center my-10 py-4 px-2">
-        <div className="w-10 h-10 rounded-full">
+        <div className="w-10 h-10 rounded-full overflow-hidden">
           <img src={miniImg} alt="" className="w-full h-full" />
         </div>
         <p className="text-[16px] leading-10 font-light">{title}</p>
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import CarouselSections from "../components/home-screen/CarouselSections";
 import DiscoverHeader from "../components/home-screen/DiscoverHeader";
 import HomeCarousel from "../components/home-screen/HomeCarousel";
+import TopCollection from "../components/home-screen/TopCollection";
 import { homeCarouselImages as Sliders }from "../assets/data";
+import { homeMiniImage as miniSliders }from "../assets/data";
 
 const HomeScreen = () => {
-  console.log(Sliders[5])
+  
   return (
     <div>
       <section className="flex w-full justify-center h-screen pt-5">
@@ -13,25 +15,30 @@ const HomeScreen = () => {
        <div className="w-full">
        <DiscoverHeader caption={"Discover"}/>
           <HomeCarousel 
-          slide1={<CarouselSections headerOne={"Get smater with"} headerTwo={"productive quizzico"} title={"Titus Katmura"} image={Sliders[0]}/>}
-          slide2={<CarouselSections headerOne={"Great Ideas comes"} headerTwo={"from brilliant mindset"} title={"Alfonzo Schuesster"} image={Sliders[1]}/>}
-          slide3={<CarouselSections headerOne={"up ideas, worth"} headerTwo={"greater height to attain"} title={"John Romauld" } image={Sliders[4]}/>}
+          slide1={<CarouselSections headerOne={"Get smater with"} headerTwo={"productive quizzico"} title={"Titus Katmura"} image={Sliders[0]} miniImg={miniSliders[0]} />}
+          slide2={<CarouselSections headerOne={"Great Ideas comes"} headerTwo={"from brilliant mindset"} title={"Alfonzo Schuesster"} image={Sliders[1]} miniImg={miniSliders[2]}/>}
+          slide3={<CarouselSections headerOne={"up ideas, worth"} headerTwo={"greater height to attain"} title={"John Romauld" } image={Sliders[4]} miniImg={miniSliders[1]}/>}
           />
+       </div>
+       <div>
+       <DiscoverHeader caption={"Top Collections"}/>
+       <TopCollection/>
+
        </div>
         <div>
         <DiscoverHeader caption={"Trending Quiz "}/>  
         <HomeCarousel 
-          slide1={<CarouselSections headerOne={"Get smater with"} headerTwo={"productive quizzico"} title={"Titus Katmura"} image={Sliders[2]}/>}
-          slide2={<CarouselSections headerOne={"Great Ideas comes"} headerTwo={"from brilliant mindset"} title={"Alfonzo Schuesster"} image={Sliders[5]}/>}
-          slide3={<CarouselSections headerOne={"up ideas, worth"} headerTwo={"greater height to attain"} title={"John Romauld"} image={Sliders[6]}/>}
+          slide1={<CarouselSections headerOne={"Get smater with"} headerTwo={"productive quizzico"} title={"Titus Katmura"} image={Sliders[2]} miniImg={miniSliders[3]}/>}
+          slide2={<CarouselSections headerOne={"Great Ideas comes"} headerTwo={"from brilliant mindset"} title={"Alfonzo Schuesster"} image={Sliders[5]} miniImg={miniSliders[1]}/>}
+          slide3={<CarouselSections headerOne={"up ideas, worth"} headerTwo={"greater height to attain"} title={"John Romauld"} image={Sliders[6]} miniImg={miniSliders[4]}/>}
           />
         </div>
         <div>
         <DiscoverHeader caption={"Top Pics"}/>
         <HomeCarousel 
-          slide1={<CarouselSections headerOne={"Get smater with"} headerTwo={"productive quizzico"} title={"Titus Katmura"} image={Sliders[3]}/>}
-          slide2={<CarouselSections headerOne={"Great Ideas comes"} headerTwo={"from brilliant mindset"} title={"Alfonzo Schuesster"} image={Sliders[7]}/>}
-          slide3={<CarouselSections headerOne={"up ideas, worth"} headerTwo={"greater height to attain"} title={"John Romauld"} image={Sliders[8]}/>}
+          slide1={<CarouselSections headerOne={"Get smater with"} headerTwo={"productive quizzico"} title={"Titus Katmura"} image={Sliders[3]} miniImg={miniSliders[0]}/>}
+          slide2={<CarouselSections headerOne={"Great Ideas comes"} headerTwo={"from brilliant mindset"} title={"Alfonzo Schuesster"} image={Sliders[7]} miniImg={miniSliders[5]}/>}
+          slide3={<CarouselSections headerOne={"up ideas, worth"} headerTwo={"greater height to attain"} title={"John Romauld"} image={Sliders[8]} miniImg={miniSliders[2]}/>}
           />
         </div>
         </div>
