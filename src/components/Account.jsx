5db292012f4c04e6c892cc82d74f93c3afb064e3
collins <kevin1520smart@gmail.com>
2023-01-19 00:19:48 +0100

    imported ProgressBar into Account

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import ProgressBar from './ProgressBar';
 
 
 const Account = ()=>{
     return(
         <div>
-            <div></div>
+            <div>
+                <ProgressBar/>
+            </div>
             <p>Create an account</p>
             <p>Please complete your profile.</p>
             <p>Don't worry your data will remain private and only you can see it.</p>
             {/* <p>Please enter your username, email address,and password. If you forget it, then you have to do forgot password.</p> */}
             <div>
                 <p>Full Name</p>
-                <input type={text}/>
+                <input type="text"/>
                 <p>Full Name</p>
-                <input type={date}/>
+                <input type="date"/>
                 <p>Full Name</p>
-                <input type={number}/>
+                <input type="number"/>
                 <p>Full Name</p>
-                <input type={number}/>
+                <input type="number"/>
             </div>
             <button>Continue</button>
         </div>
