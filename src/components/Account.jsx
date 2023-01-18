32571925552de0823117286dafe606c20963f991
collins <kevin1520smart@gmail.com>
2023-01-18 23:43:29 +0100

    working on Account

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 
 const Account = ()=>{
     return(
-        <div></div>
+        <div>
+            <div></div>
+            <p>Create an account</p>
+            <p>Please complete your profile.</p>
+            <p>Don't worry your data will remain private and only you can see it.</p>
+            {/* <p>Please enter your username, email address,and password. If you forget it, then you have to do forgot password.</p> */}
+            <div>
+                <p>Full Name</p>
+                <input type={text}/>
+                <p>Full Name</p>
+                <input type={date}/>
+                <p>Full Name</p>
+                <input type={number}/>
+                <p>Full Name</p>
+                <input type={number}/>
+            </div>
+            <button>Continue</button>
+        </div>
     )
 }
 
