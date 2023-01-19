5253e9312ee63476e6728e7aa39bb55ce58e7a26
collins <kevin1520smart@gmail.com>
2023-01-19 04:19:03 +0100

    work still in progress

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Inputs from "./Inputs";
 import ProgressBar from "./ProgressBar";
+import { useDispatch} from "react-redux";
+import { incrementByamount } from "../redux/bar-features/barSlice";
 
 const Account = () => {
+  const dispatch = useDispatch();
+  
+  const handleProgessWidth =()=>{
+    dispatch(incrementByamount())
+  }
+
   return (
     <div className="text-center px-2">
-      {/* <div>
-                <ProgressBar/>
-            </div> */}
-      <p className="font-bold text-3xl">Create an account✏</p>
+      <div className="">
+          <ProgressBar/>
+      </div>
+      <p className="font-bold text-2xl mb-3">Create an account✏</p>
       <p>Please complete your profile.</p>
       <p>Don't worry your data will remain private and only you can see it.</p>
       {/* <p>Please enter your username, email address,and password. If you forget it, then you have to do forgot password.</p> */}
@@ -19,7 +27,7 @@ const Account = () => {
         <Inputs title="Country" type="text" option={true} />
         <Inputs title="Age" type="number" option={true} />
       </div>
-      <button className="bg-[#6949ff] text-white w-full py-2 rounded-full my-4">
+      <button onClick={handleProgessWidth} className="bg-[#6949ff] text-white w-full py-2 rounded-full my-4">
         Continue
       </button>
     </div>
