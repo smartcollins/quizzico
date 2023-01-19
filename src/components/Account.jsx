a97ecb045d6c74968577a738a1ce377a3ae04575
collins <kevin1520smart@gmail.com>
2023-01-19 01:00:07 +0100

    still working on Account

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,54 +1,29 @@
-import React from 'react';
-import Inputs from './Inputs';
-import ProgressBar from './ProgressBar';
+import React from "react";
+import Inputs from "./Inputs";
+import ProgressBar from "./ProgressBar";
 
-
-const Account = ()=>{
-    return(
-        <div className='text-center px-2'>
-            {/* <div>
+const Account = () => {
+  return (
+    <div className="text-center px-2">
+      {/* <div>
                 <ProgressBar/>
             </div> */}
-            <p className='font-bold text-3xl'>Create an account✏</p>
-            <p>Please complete your profile.</p>
-            <p>Don't worry your data will remain private and only you can see it.</p>
-            {/* <p>Please enter your username, email address,and password. If you forget it, then you have to do forgot password.</p> */}
-            <div className='text-left'>
-                <Inputs
-                    title="Full Name"
-                    type="text"
-                />
-                <Inputs
-                    title="Date of Birth"
-                    type="date"
-                />
-                <Inputs
-                    title="Phone Number"
-                    type="text"
-                />
-                <Inputs
-                    title="Country"
-                    type="text"
-                    option={true}
-                />
-                <Inputs
-                    title="Age"
-                    type="number"
-                    option={true}
-                />
-                {/* <p className='font-bold'>Full Name</p>
-                <input className=' border-gray-400 border-b-2 b-2' type="text"/>
-                <p>Full Name</p>
-                <input type="date"/>
-                <p>Full Name</p>
-                <input type="number"/>
-                <p>Full Name</p>
-                <input type="number"/> */}
-            </div>
-            <button>Continue</button>
-        </div>
-    )
-}
+      <p className="font-bold text-3xl">Create an account✏</p>
+      <p>Please complete your profile.</p>
+      <p>Don't worry your data will remain private and only you can see it.</p>
+      {/* <p>Please enter your username, email address,and password. If you forget it, then you have to do forgot password.</p> */}
+      <div className="text-left">
+        <Inputs title="Full Name" type="text" />
+        <Inputs title="Date of Birth" type="date" />
+        <Inputs title="Phone Number" type="text" />
+        <Inputs title="Country" type="text" option={true} />
+        <Inputs title="Age" type="number" option={true} />
+      </div>
+      <button className="bg-[#6949ff] text-white w-full py-2 rounded-full my-4">
+        Continue
+      </button>
+    </div>
+  );
+};
 
-
-export default Account; 
+export default Account;
