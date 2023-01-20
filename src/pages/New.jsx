4934c870d7218e36149120dbb5fb77a2a085537e
collins <kevin1520smart@gmail.com>
2023-01-20 23:14:15 +0100

    modified New

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Purpose from '../components/Purpose';
 
 
 const New = ()=>{
     return(
         <div className='px-2'>
-            
+            <Purpose
+                txt="Create new password 🔐"
+                extra={true}
+                txt2="Save the new password in asafe place, if you forget it then you have to do a forgot password again."
+                new_pass={true}
+                btn_txt="Confirm"
+            />
         </div>
     )
 }
