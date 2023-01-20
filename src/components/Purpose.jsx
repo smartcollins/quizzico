bc873c1486a64baf250b6492b685c9b1ad7a82c5
collins <kevin1520smart@gmail.com>
2023-01-20 22:35:37 +0100

    modified Purpose

--- src/components/Purpose.jsx
+++ src/components/Purpose.jsx
@@ -3,7 +3,7 @@ import Inputs from './Inputs';
 import { ArrowLeft } from 'phosphor-react';
 
 
-const Purpose = ({txt,extra,txt2,input,input2,pass,forget,btn_txt})=>{
+const Purpose = ({txt,extra,txt2,pass,forget,mail,mail_txt,mail_txt2,btn_txt})=>{
     return(
         <div>
             <ArrowLeft size={30} color="#000000" weight="light" />
@@ -29,10 +29,22 @@ const Purpose = ({txt,extra,txt2,input,input2,pass,forget,btn_txt})=>{
                     {forget&&<p className='text-[#694bff] text-center mx-auto font-medium'>Forgot Password?</p>}
                 </div>:
                 <div>
-                    <Inputs
+                    {mail?null:<Inputs
                         title="Email"
                         type="email"
-                    />
+                    />}
+                </div>
+            }
+            {mail&&
+                <div className='text-center'>
+                    <div className='flex items-center justify-between mx-auto w-11/12 my-4'>
+                        <button className='border-transparent bg-gray-100 rounded-md w-12 h-12'>4</button>
+                        <button className='border-transparent bg-gray-100 rounded-md w-12 h-12'>6</button>
+                        <button className='border-transparent bg-gray-100 rounded-md w-12 h-12'>7</button>
+                        <button className='border-transparent bg-gray-100 rounded-md w-12 h-12'></button>
+                    </div>
+                    <p className='my-4 mt-8'>{mail_txt}</p>
+                    <p>{mail_txt2} <span>55</span> s</p>
                 </div>
             }
             <div className='w-full text-center fixed bottom-0'>
