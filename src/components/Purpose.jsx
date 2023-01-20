b4912ef4f1c82386df1235451805334e8d8fff8b
collins <kevin1520smart@gmail.com>
2023-01-20 14:21:42 +0100

    modified Purpose

--- src/components/Purpose.jsx
+++ src/components/Purpose.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import Inputs from './Inputs';
 
 
-const Purpose = ()=>{
+const Purpose = ({txt,txt2,input,input2,pass,forget,btn_txt})=>{
     return(
-        <div></div>
+        <div>
+            <p>{txt}</p>
+            {
+                pass?
+                <div>
+                    <Inputs
+                        title="Email"
+                        type="email"
+                    />
+                    <Inputs
+                        title="Password"
+                        type="password"
+                    />
+                    <div>
+                        <input id='check' type="checkbox"/>
+                        <label htmlFor='check'>Remember me</label>
+                    </div>
+                    {forget&&<p>Forgot Password</p>}
+                </div>:
+                <div>
+                    <Inputs
+                        title="Email"
+                        type="email"
+                    />
+                </div>
+            }
+            <button>{btn_txt}</button>
+        </div>
     )
 }
 
