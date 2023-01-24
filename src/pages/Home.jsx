6228a6b5b60e3066d63c05bc5317732e280c2168
collins <kevin1520smart@gmail.com>
2023-01-24 23:07:28 +0100

    imported BtmNavbar into Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import BtmNavbar from '../components/BtmNavbar';
 import Navbar from '../components/Navbar';
+// import Play from '../components/Play';
 
 
 const Home = ()=>{
     return(
         <div>
             <Navbar/>
+            {/* <Play/> */}
+            <BtmNavbar/>
         </div>
     )
 }
