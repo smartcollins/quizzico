5b1bab0aac3201230436f50ea8f9044ccd21769d
collins <kevin1520smart@gmail.com>
2023-01-25 21:03:20 +0100

    modified Play

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -4,8 +4,10 @@ import React from 'react';
 const Play = ()=>{
     return(
         <div className='bg-[#6949ff] h-[30vh] rounded-xl px-2 mx-2'>
-            <p>Play quiz together with your friends now!</p>
-            <button>Find Friends</button>
+            <div className='bg-red-500 w-2/4 h-full'>
+                <p>Play quiz together with your friends now!</p>
+                <button>Find Friends</button>
+            </div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Authors from '../components/Authors';
 import BtmNavbar from '../components/BtmNavbar';
 import Navbar from '../components/Navbar';
-// import Play from '../components/Play';
+import Play from '../components/Play';
 
 
 const Home = ()=>{
     return(
         <div>
             <Navbar/>
-            {/* <Play/> */}
+            <Play/>
             <Authors/>
             <BtmNavbar/>
         </div>
