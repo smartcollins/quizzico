ce96df8cd42382f6613e4c05e9c53d5b72a63771
collins <kevin1520smart@gmail.com>
2023-01-25 20:29:46 +0100

    modifying Authors

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -9,28 +9,30 @@ import { ArrowRight } from 'phosphor-react';
 const Authors = ()=>{
     return(
         <div>
-            <div>
+            <div className='flex items-center justify-between'>
                 <p>Top Authors</p>
-                <div>
+                <div className='flex items-center justify-between'>
                     <p>View all</p>
                     <ArrowRight size={25} color="#6345f0" />
                 </div>
             </div>
-            <div>
-                <img src={author} alt="user"/>
-                <p>Rayford</p>
-            </div>
-            <div>
-                <img src={author2} alt="user"/>
-                <p>Willard</p>
-            </div>
-            <div>
-                <img src={author3} alt="user"/>
-                <p>Hannah</p>
-            </div>
-            <div>
-                <img src={author4} alt="user"/>
-                <p>Geoffrey</p>
+            <div className='flex items-center justify-between'>
+                <div className='flex flex-col items-center'>
+                    <img  className=' w-2/4 md:w-full' src={author} alt="user"/>
+                    <p className=' text-lg font-medium'>Rayford</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <img  className=' w-2/4 md:w-full' src={author2} alt="user"/>
+                    <p className=' text-lg font-medium'>Willard</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <img  className=' w-2/4 md:w-full' src={author3} alt="user"/>
+                    <p className=' text-lg font-medium'>Hannah</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <img  className=' w-2/4 md:w-full' src={author4} alt="user"/>
+                    <p className=' text-lg font-medium'>Geoffrey</p>
+                </div>
             </div>
         </div>
     )
