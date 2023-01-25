98e08b4a7bfc19e36c85ee9f571b23fae377af38
collins <kevin1520smart@gmail.com>
2023-01-25 21:32:22 +0100

    work in progress

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 
 const Play = ()=>{
     return(
-        <div className='bg-[#6949ff] h-[30vh] rounded-xl px-2 mx-2'>
+        <div className='bg-[#6949ff] h-1/2 rounded-xl px-2 mx-2'>
             <div className='bg-red-500 py-2 w-2/4 h-full'>
                 <p className='text-white font-semibold text-xl'>Play quiz together with your friends now!</p>
                 <button className=' text-[#6949ff] bg-white w-3/4 py-1 rounded-full'>Find Friends</button>
             </div>
+            {/* <p>🎈👩‍🦰👩🏾🙅🏾‍♀️🙆🏻‍♀️</p> */}
         </div>
     )
 }
