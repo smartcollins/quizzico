c53afb01cdc132a6444477ac41a1105efd4d1048
collins <kevin1520smart@gmail.com>
2023-02-09 17:53:38 +0100

    cleaned up SignIn

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import Purpose from '../components/Purpose';
+import React from "react";
+import Purpose from "../components/Purpose";
 
+const SignIn = () => {
+  return (
+    <div className="px-2">
+      <Purpose
+        place="/"
+        txt="Hello there 👋"
+        pass={true}
+        forget={true}
+        btn_txt="SIGN IN"
+      />
+    </div>
+  );
+};
 
-const SignIn = ()=>{
-    return(
-        <div className='px-2'>
-            <Purpose
-                txt="Hello there 👋"
-                pass={true}
-                forget={true}
-                btn_txt="SIGN IN"
-            />
-        </div>
-    )
-}
-
-
-export default SignIn; 
+export default SignIn;
