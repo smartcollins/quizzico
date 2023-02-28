fa1c2580e1a64c3c4bbb562e4c5ac06027d1ac0c
Elochukwu3 <oguajuromauld5@gmail.com>
2023-02-28 17:54:48 +0100

    update: update menu and nav

---
+++ src/components/library/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>u
+        <div>
+            <h3 className='capitalize'>my quizzo</h3>
+            <h3 className='capitalize'>favorites</h3>
+            <h3 className='capitalize'>collaboration</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Menu
--- src/components/library/Nav.jsx
+++ src/components/library/Nav.jsx
@@ -4,7 +4,7 @@ import {MagnifyingGlass } from 'phosphor-react';
 const Nav = () => {
   return (
     <div className='flex items-center justify-between'>
-    <div className='flex items-center w-2/4 bg-red-400'>
+    <div className='flex items-center w-2/4'>
         <img className='w-1/5 max-sm:w-1/3' src={logo} alt="logo"/>
         <p className=' text-xl font-bold capitalize'>library</p>
     </div>
