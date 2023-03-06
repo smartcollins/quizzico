fd91dd7b17341ea988b6ce6e7f0f4b94ff0ed3e0
collins <kevin1520smart@gmail.com>
2023-03-06 23:44:18 +0100

    formatted Search

--- src/components/home-screen/Search.jsx
+++ src/components/home-screen/Search.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import {value} from "../../assets/value";
+import { value } from "../../assets/value";
 import { category, peopleYouMayKnow, collections } from "../../assets/data"
 import SearchPeople from './SearchPeople';
 import { ArrowLeft, MagnifyingGlass, X } from 'phosphor-react';
 
-const Search = ()=>{
+const Search = () => {
     const sm = value.sm
     const md = value.md
 
@@ -14,34 +14,34 @@ const Search = ()=>{
     const [ple, setPle] = useState(false);
     const [lt, setLt] = useState(false);
 
-    const onCat = ()=>{
+    const onCat = () => {
         setCat(true)
         setQz(true)
     }
-    const onQz = ()=>{
+    const onQz = () => {
         setQz(true)
         setPle(false)
         setLt(false)
     }
-    const onPle = ()=>{
+    const onPle = () => {
         setPle(true)
         setQz(false)
         setLt(false)
     }
-    const onLt = ()=>{
+    const onLt = () => {
         setLt(true)
         setPle(false)
         setQz(false)
     }
 
-    const item = category.map((item,idx)=>(<div key={idx} className={item.bold?' border-b border-[#757575] flex items-center justify-between my-4 py-2':'flex items-center justify-between my-4'}><p onClick={onCat} className=' capitalize text-[#757575] font-medium text-lg'>{item.txt}</p><X size={sm} color="#757575" /></div>))
-    const item2 = peopleYouMayKnow.map((item,idx)=><SearchPeople key={idx} {...item}/>)
+    const item = category.map((item, idx) => (<div key={idx} className={item.bold ? ' border-b border-[#757575] flex items-center justify-between my-4 py-2' : 'flex items-center justify-between my-4'}><p onClick={onCat} className=' capitalize text-[#757575] font-medium text-lg'>{item.txt}</p><X size={sm} color="#757575" /></div>))
+    const item2 = peopleYouMayKnow.map((item, idx) => <SearchPeople key={idx} {...item} />)
     // const item3 = collections.map((item,idx)=><div className="grid grid-cols-2 gap-2 md:flex md:flex-col md:items-center md:gap-4"><img key={idx} className="md:object-cover md:w-4/5" src={item.img} alt="collections"/></div>)
-    const item3 = collections.map((item,idx)=><img key={idx} className="md:object-cover md:w-4/5" src={item.img} alt="collections"/>)
+    const item3 = collections.map((item, idx) => <img key={idx} className="md:object-cover md:w-4/5" src={item.img} alt="collections" />)
 
 
 
-    return(
+    return (
         <div className='py-4 px-2'>
             <div className='flex gap-4 items-center'>
                 <Link to={"/home"}>
@@ -49,24 +49,19 @@ const Search = ()=>{
                 </Link>
                 <div className="flex items-center bg-[#f4f2ff] w-full p-2 rounded-lg border focus-within:border-[#6a4aff] ">
                     <MagnifyingGlass size={md} color="#6a4aff" />
-                    <input className=' bg-transparent outline-none ml-2 w-full' placeholder='Category'/>
+                    <input className=' bg-transparent outline-none ml-2 w-full' placeholder='Category' />
                     <X size={sm} color="#000000" weight='bold' />
                 </div>
             </div>
             <div>
-                {cat&&<div className='flex justify-between items-center gap-4 my-4'>
-                    <button onClick={onQz} className={qz?"bg-[#6949ff] text-white rounded-3xl w-2/4":" bg-transparent text-[#6949ff] border border-[#6949ff] rounded-3xl w-2/4"}>Quiz</button>
-                    <button onClick={onPle} className={ple?"bg-[#6949ff] text-white rounded-3xl w-2/4":" bg-transparent text-[#6949ff] border border-[#6949ff] rounded-3xl w-2/4"}>People</button>
-                    <button onClick={onLt} className={lt?"bg-[#6949ff] text-white rounded-3xl w-2/4":" bg-transparent text-[#6949ff] border border-[#6949ff] rounded-3xl w-2/4"}>Collections</button>
+                {cat && <div className='flex justify-between items-center gap-4 my-4'>
+                    <button onClick={onQz} className={qz ? "bg-[#6949ff] text-white rounded-3xl w-2/4" : " bg-transparent text-[#6949ff] border border-[#6949ff] rounded-3xl w-2/4"}>Quiz</button>
+                    <button onClick={onPle} className={ple ? "bg-[#6949ff] text-white rounded-3xl w-2/4" : " bg-transparent text-[#6949ff] border border-[#6949ff] rounded-3xl w-2/4"}>People</button>
+                    <button onClick={onLt} className={lt ? "bg-[#6949ff] text-white rounded-3xl w-2/4" : " bg-transparent text-[#6949ff] border border-[#6949ff] rounded-3xl w-2/4"}>Collections</button>
                 </div>}
-                {/* {
-                    cat?
-                    <div>{item2}</div>:
-                    <div>{item}</div>
-                } */}
-                {!cat&&<div>{item}</div>}
-                {qz&&<div>{item2}</div>}
-                {lt&&<div className="grid grid-cols-2 gap-2 md:flex md:flex-col md:items-center md:gap-4">{item3}</div>}
+                {!cat && <div>{item}</div>}
+                {ple && <div>{item2}</div>}
+                {lt && <div className="grid grid-cols-2 gap-2 md:flex md:flex-col md:items-center md:gap-4">{item3}</div>}
             </div>
         </div>
     )
