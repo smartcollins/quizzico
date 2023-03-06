c4f8ed77e497a7cee9c5085d4d084f6a861c52d0
collins <kevin1520smart@gmail.com>
2023-03-06 23:23:24 +0100

    modifying top button in  Search

--- src/components/home-screen/Search.jsx
+++ src/components/home-screen/Search.jsx
@@ -7,12 +7,31 @@ import { ArrowLeft, MagnifyingGlass, X } from 'phosphor-react';
 
 const Search = ()=>{
     const [cat, setCat] = useState(false);
+    const [qz, setQz] = useState(false);
+    const [ple, setPle] = useState(false);
+    const [lt, setLt] = useState(false);
 
     const sm = value.sm
     const md = value.md
 
     const onCat = ()=>{
         setCat(true)
+        setQz(true)
+    }
+    const onQz = ()=>{
+        setQz(true)
+        setPle(false)
+        setLt(false)
+    }
+    const onPle = ()=>{
+        setPle(true)
+        setQz(false)
+        setLt(false)
+    }
+    const onLt = ()=>{
+        setLt(true)
+        setPle(false)
+        setQz(false)
     }
 
     const item = category.map((item,idx)=>(<div key={idx} className={item.bold?' border-b border-[#757575] flex items-center justify-between my-4 py-2':'flex items-center justify-between my-4'}><p onClick={onCat} className=' capitalize text-[#757575] font-medium text-lg'>{item.txt}</p><X size={sm} color="#757575" /></div>))
@@ -33,15 +52,17 @@ const Search = ()=>{
                 </div>
             </div>
             <div>
+                <div className='flex justify-between items-center gap-4 my-4'>
+                    <button onClick={onQz} className={qz?"bg-[#6949ff] text-white rounded-3xl w-2/4":" bg-transparent text-[#6949ff] border border-[#6949ff] rounded-3xl w-2/4"}>Quiz</button>
+                    <button onClick={onPle} className={ple?"bg-[#6949ff] text-white rounded-3xl w-2/4":" bg-transparent text-[#6949ff] border border-[#6949ff] rounded-3xl w-2/4"}>People</button>
+                    <button onClick={onLt} className={lt?"bg-[#6949ff] text-white rounded-3xl w-2/4":" bg-transparent text-[#6949ff] border border-[#6949ff] rounded-3xl w-2/4"}>Collections</button>
+                </div>
                 {
                     cat?
                     <div>{item2}</div>:
                     <div>{item}</div>
                 }
             </div>
-            {/* <div>
-                {item}
-            </div> */}
         </div>
     )
 }
