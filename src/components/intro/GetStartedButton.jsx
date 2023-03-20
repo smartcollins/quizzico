927c610e5509a923d941079115b1bbb92a5c1507
Elochukwu3 <oguajuromauld5@gmail.com>
2023-03-20 21:37:00 +0100

    Fix: Remove static values from GetStartedButton

--- src/components/intro/GetStartedButton.jsx
+++ src/components/intro/GetStartedButton.jsx
@@ -5,14 +5,14 @@ import { ROUTE_PATHS } from "../../assets/data_two";
 
 const GetStartedButton = () => {
   return (
-    <div className="uppercase text-white space-y-4 mt-10 max-sm:mt-15 max-sm:space-y-5 w-full flex items-center flex-col justify-center">
-      <Link to={ROUTE_PATHS.OCCUPATION} className="w-[270px] max-sm:w-[90%]">
-        <button className=" shadow-sm shadow-[#6949ff] px-3 w-full uppercase text-sm bg-[#6949ff] block py-3 rounded-[30px]  hover:opacity-75 hover:bg-white hover:text-[#6949ff] hover:border-2 border-[#6949ff]">
+    <div className=" uppercase text-white space-y-4 mt-10 max-sm:mt-15 max-sm:space-y-5 w-full flex items-center flex-col justify-center">
+      <Link to={ROUTE_PATHS.OCCUPATION} className="w-270px max-sm:w-11/12">
+        <button className=" shadow-sm shadow-purple px-3 w-full uppercase text-sm bg-purple block py-3 rounded-full  hover:opacity-75 hover:bg-white hover:text-purple hover:border-2 border-purple ">
           get started
         </button>
       </Link>
-      <Link to={ROUTE_PATHS.SIGNIN} className="w-[270px] max-sm:w-[90%]">
-        <button className=" shadow-sm shadow-[#bbb] px-3 w-full uppercase text-sm text-[#6949ff]  py-3 rounded-[30px] block  bg-[#f0edff] hover:opacity-75 border-2 border-[#6949ff]">
+      <Link to={ROUTE_PATHS.SIGNIN} className="w-270px max-sm:w-11/12">
+        <button className=" shadow-sm shadow-purple-100 px-3 w-full uppercase text-sm text-purple  py-3 rounded-full block  bg-lwhite hover:opacity-75 border-2 border-purple border-unset">
           i already have an account
         </button>
       </Link>
