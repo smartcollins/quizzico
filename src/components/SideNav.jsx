b2b090aff1d2cc9b59efbeb6d0a3fb06904c6c4e
collins <kevin1520smart@gmail.com>
2023-04-13 16:00:29 +0100

    cleaned up SideNav

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,54 +1,27 @@
 import React from 'react';
 import logo from "../images/quizzo-logo.png"
 import author from '../images/author.jpg'
-import {value} from '../assets/value'
-import { sideNavAry } from '../assets/data';
-// import { ChartBar, ChatText, CheckSquare, ClipboardText, Gear, Horse, House, MagnifyingGlass, Newspaper, SignOut, Tray } from 'phosphor-react';
+import { value } from '../assets/value'
+import { sideNavAry } from '../assets/data'
+
 import { MagnifyingGlass, SignOut } from 'phosphor-react';
 
 // link to the figma design sidebar
 
 // https://www.figma.com/file/cmry3u8WTZy0wiG6RIpd86/Sidebar-Navigate---MingCute-Icon-(Community)?node-id=41-138&t=t9c6u8EBFzTp0GnO-0
 
-// const ary = [
-                
-//                 {    icon:House,
-//                     txt: "Home"},
-                
-//                 {    icon:ClipboardText,
-//                     txt: "Orders"},
-                
-//                 {    icon:CheckSquare,
-//                     txt: "Schedule"},
-                
-//                 {    icon:ChatText,
-//                     txt: "Messages"},
-                
-//                 {    icon:Tray,
-//                     txt: "inbox"},
-                
-//                 {    icon:ChartBar,
-//                     txt: "analytics"},
-                
-//                 {    icon:Newspaper,
-//                     txt: "news"},
-                
-//                 {    icon:Gear,
-//                     txt: "settings"},
-// ]
-
-const item = sideNavAry.map((item,idx)=><div key={idx} className='flex items-center gap-2'>
+const item = sideNavAry.map((item, idx) => <div key={idx} className='flex items-center gap-2'>
     {
-        React.createElement(item.icon,{
-            color:'#000000', size:20
+        React.createElement(item.icon, {
+            color: '#000000', size: 20
         })
     }
     <p>{item.txt}</p>
 </div>)
 
 
-const SideNav = ()=>{
-    return(
+const SideNav = () => {
+    return (
         <div className='relative h-screen ml-4'>
             <div className='flex items-center gap-2'>
                 <img className=' h-12' src={logo} alt="logo" />
@@ -59,7 +32,7 @@ const SideNav = ()=>{
                 <input className=' outline-none bg-transparent' type="text" placeholder='Search' />
             </div>
             {/* <div className=' space-y-3 m-4 text-lg capitalize bg-red-500 h-[70%]'> */}
-            <div className=' flex flex-col justify-between m-4 text-lg capitalize bg-red-500 h-[60%] xl:h-[70%] 2xl:h-[80%]'>
+            <div className=' flex flex-col justify-between m-4 text-lg capitalize h-3/5 xl:h-[70%] 2xl:h-4/5'>
                 {item}
             </div>
             <div className='flex items-center justify-between w-full absolute bottom-2'>
