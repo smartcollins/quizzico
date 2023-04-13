dfe5fa66ae3b2ff92897a029392eab29b6be85ba
collins <kevin1520smart@gmail.com>
2023-04-13 16:19:07 +0100

    minor changes made in Play

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -12,25 +12,28 @@ import frd8 from "../images/frd8.jpg";
 
 const Play = () => {
   return (
-    <div className="relative bg-[#785bff] h-[30vh]  rounded-xl px-2 mx-2 md:h-[15vh]">
+    // <div className="relative bg-[#785bff] h-[30vh]  rounded-xl px-2 mx-2 md:h-[15vh]">
+    <div className="relative bg-[#785bff] h-[30vh]  rounded-xl px-2 mx-2 md:h-[40vh] lg:h-[45vh]">
       <div className="py-2 w-2/3 h-full">
-        <p className="text-white font-semibold text-xl w-full md:text-2xl">
+        {/* <p className="text-white font-semibold text-xl w-full md:text-2xl"> */}
+        <p className="text-white font-semibold text-xl w-full md:text-3xl md:w-[55%] lg:text-4xl bg-red-400">
           Play quiz together with your friends now!
         </p>
         <Link to="/find-friends">
-          <button className=" text-[#6949ff] bg-white w-3/4 my-4 py-1 rounded-full">
+          {/* <button className=" text-[#6949ff] bg-white w-3/4 my-4 py-1 rounded-full"> */}
+          <button className=" text-[#6949ff] bg-white w-3/4 my-4 py-1 rounded-full md:w-[60%] md:py-2">
             Find Friends
           </button>
         </Link>
       </div>
-      <img className="absolute top-2 right-4" src={frd} alt="icons" />
-      <img className="absolute top-4 right-[20%]" src={frd2} alt="icons" />
-      <img className="absolute top-14 right-[20%]" src={frd3} alt="icons" />
-      <img className="absolute top-10 right-[10%]" src={frd4} alt="icons" />
-      <img className="absolute top-20 right-5" src={frd5} alt="icons" />
-      <img className="absolute bottom-8 right-0" src={frd6} alt="icons" />
-      <img className="absolute bottom-4 right-[20%]" src={frd7} alt="icons" />
-      <img className="absolute bottom-2 right-[10%]" src={frd8} alt="icons" />
+      <img className="absolute top-2 right-4 lg:h-12" src={frd} alt="icons" />
+      <img className="absolute top-4 right-[20%] lg:h-12" src={frd2} alt="icons" />
+      <img className="absolute top-14 right-[20%] lg:h-12" src={frd3} alt="icons" />
+      <img className="absolute top-10 right-[10%] lg:h-12" src={frd4} alt="icons" />
+      <img className="absolute top-20 right-5 lg:h-12" src={frd5} alt="icons" />
+      <img className="absolute bottom-8 right-0 lg:h-12" src={frd6} alt="icons" />
+      <img className="absolute bottom-4 right-[20%] lg:h-12" src={frd7} alt="icons" />
+      <img className="absolute bottom-2 right-[10%] lg:h-12" src={frd8} alt="icons" />
     </div>
   );
 };
