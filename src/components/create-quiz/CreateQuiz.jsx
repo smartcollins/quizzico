1015d95e4dd1b92d4be95e2dd6c02e9fd95dd50a
Elochukwu3 <oguajuromauld5@gmail.com>
2023-04-22 18:06:45 +0100

    Feat: Create CreateQuiz page

---
+++ src/components/create-quiz/CreateQuiz.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import AddImage from '../AddImage'
+import Inputs from '../Inputs'
+
+const CreateQuiz = () => {
+  return (
+    <div className=" w-full">
+    <div className="mx-auto   w-3/6 max-sm:w-11/12">
+    <AddImage/>
+    <Inputs 
+    title={"Title"}
+    placeholder={"Enter a Collection Title"}
+    type={"text"}
+    />
+    <Inputs 
+    title={"Description"}
+    placeholder={"Enter Description"}
+    type={"text"}
+    />
+    <DropDown
+     title={"Select Collection"}
+    //  handleDropAction={handleDropAction}
+    //  dropAction={dropArrow}
+     contents={["Only Me", "Public"]}
+    />
+    <DropDown
+     title={"Select Theme"}
+    //  handleDropAction={handleDropAction}
+    //  dropAction={dropArrow}
+     contents={["Only Me", "Public"]}
+    />
+    <DropDown
+     title={"Select Collection"}
+    //  handleDropAction={handleDropAction}
+    //  dropAction={dropArrow}
+     contents={["Only Me", "Public"]}
+    />
+    <DropDown
+     title={"Visibility of quiz questions"}
+    //  handleDropAction={handleDropAction}
+    //  dropAction={dropArrow}
+     contents={["Only Me", "Public"]}
+    />
+     <Inputs 
+    title={"Keyword"}
+    placeholder={"Enter Description"}
+    type={"text"}
+    />
+    </div>
+    </div>
+  )
+}
+
+export default CreateQuiz
