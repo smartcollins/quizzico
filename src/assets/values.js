36ed11e264e71934884ff57d20a6c7c73edc6239
collins <kevin1520smart@gmail.com>
2023-07-28 21:32:06 +0100

    created  values.js

---
+++ src/assets/values.js
@@ -0,0 +1,24 @@
+// all hard coded values for each sections in the Figma design would be represented.
+
+const values = [
+    {
+        sect: 3,
+        height: '30vh'
+    },{
+        sect :5,
+        color: {
+            right:'#25d597',
+            wrong:'#f75959'
+        }
+
+
+    }
+]
+
+// const color = {
+//     section5:{
+        
+//     }
+// }
+
+export default values;
