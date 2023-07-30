86d2de6a37a931ea3c0615ff9ede348d23334f30
collins <kevin1520smart@gmail.com>
2023-07-30 19:56:19 +0100

    minor modification made in Factor

--- src/components/005/Factor.jsx
+++ src/components/005/Factor.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import values from '../../assets/values';
 
 const Factor = ({bad})=>{
+    const height = values.sect_5.height
     const {right,wrong} = values.sect_5.color
+    console.log(height)
+    // const me = 'h-[70vh]'
+    // const m = '[50vh]'
 
     return(
-        <div style={{backgroundColor: bad?wrong:right}} className='h-[30vh] relative' >
+        // <div style={{backgroundColor: bad?wrong:right}} className={`${me} relative`} >
+        // <div style={{backgroundColor: bad?wrong:right}} className={`h-${height} relative`} >
+        <div style={{backgroundColor: bad?wrong:right}} className={`${height} relative`} >
             <div className='w-2/4 text-center absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 space-y-4 font-medium'>
                 <p className='text-white text-lg capitalize'>{bad?'Incorrect!':'correct!'}</p>
                 <button style={{color: bad? wrong: right}} className='bg-white py-1 px-4 rounded-2xl'>
