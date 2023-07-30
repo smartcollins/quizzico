7c6b057d17e19bede6bd836dd8f4234abc4f390b
collins <kevin1520smart@gmail.com>
2023-07-30 18:30:45 +0100

    made values object of objects

--- src/assets/values.js
+++ src/assets/values.js
@@ -1,24 +1,15 @@
 // all hard coded values for each sections in the Figma design would be represented.
 
-const values = [
-    {
-        sect: 3,
-        height: '30vh'
-    },{
-        sect :5,
-        color: {
-            right:'#25d597',
-            wrong:'#f75959'
-        }
-
-
-    }
-]
-
-// const color = {
-//     section5:{
-        
-//     }
-// }
+const values = {
+  sect_3: {
+    height: "30vh",
+  },
+  sect_5: {
+    color: {
+      right: "#25d597",
+      wrong: "#f75959",
+    },
+  },
+};
 
 export default values;
