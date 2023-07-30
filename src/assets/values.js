ace176ce5415fbe76b59ce9c3d708f3f10108701
collins <kevin1520smart@gmail.com>
2023-07-30 19:56:35 +0100

    minpor changes in values.js

--- src/assets/values.js
+++ src/assets/values.js
@@ -1,10 +1,15 @@
 // all hard coded values for each sections in the Figma design would be represented.
 
+
+// Ref: height value different from 50vh would refuse to display.
+
 const values = {
   sect_3: {
     height: "30vh",
   },
   sect_5: {
+    // height: "[50vh]",
+    height: "h-[30vh]",
     color: {
       right: "#25d597",
       wrong: "#f75959",
