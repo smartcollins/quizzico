c73f8fc3209bfb607f2265fbec28bf04e509e364
collins <kevin1520smart@gmail.com>
2023-07-31 23:17:49 +0100

    modifying Question

--- src/components/005/Question.jsx
+++ src/components/005/Question.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import { DotsThreeOutline } from 'phosphor-react';
+import img1 from '../../images/q-1.jpg'
 
 const Question = ()=>{
     return(
-        <div></div>
+        <div>
+            <div>
+                <p>1/10</p>
+                <p>Qiuz</p>
+                <DotsThreeOutline size={32} color="#25d597" weight="bold" />
+            </div>
+            <img src={img1} alt="description" />
+            <p>.... do you get to school? By bus?</p>
+        </div>
     )
 }
 
