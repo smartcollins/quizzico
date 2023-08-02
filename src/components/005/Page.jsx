bddc577eeb7bc8cebe53fab15faf4f586bbc6679
collins <kevin1520smart@gmail.com>
2023-08-02 22:29:55 +0100

    minor modification made in Page

--- src/components/005/Page.jsx
+++ src/components/005/Page.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Factor from './Factor';
 import Question from './Question';
+import { question } from '../../assets/data';
 
 
 const Page = ()=>{
+    const item = question.map((item,idx)=><Question key={idx} {...item}/>)
     return(
         <div>
-            <Factor/>
-            <Question/>
+            {/* <Factor/> */}
+            {/* <Question/> */}
+            {item}
         </div>
     )
 }
