aab2e4029da16cd4327e9b6f6cd461b3f4d3dde1
collins <kevin1520smart@gmail.com>
2023-08-02 22:19:57 +0100

    modifiying Question

--- src/components/005/Question.jsx
+++ src/components/005/Question.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { DotsThreeOutline } from 'phosphor-react';
 import img1 from '../../images/q-1.jpg'
+import { value } from '../../assets/value';
+import { DotsThreeOutline } from 'phosphor-react';
 
 const Question = ()=>{
     return(
-        <div>
-            <div>
+        <div className='text-xl font-semibold space-y-4'>
+            <div className='flex items-center justify-between my-4'>
                 <p>1/10</p>
                 <p>Qiuz</p>
-                <DotsThreeOutline size={32} color="#25d597" weight="bold" />
+                <DotsThreeOutline className=' border-2 border-black rounded-full p-1' size={value.md} color="#000000" weight="bold" />
             </div>
-            <img src={img1} alt="description" />
-            <p>.... do you get to school? By bus?</p>
+            <img className='w-full rounded-2xl' src={img1} alt="description" />
+            <p className='text-center w-3/4 mx-auto'>.... do you get to school? By bus?</p>
         </div>
     )
 }
