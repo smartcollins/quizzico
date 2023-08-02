8e5bf9c8bd4dc03290e650894632ed0695ad2e42
collins <kevin1520smart@gmail.com>
2023-08-02 22:26:48 +0100

    made questions use Props

--- src/components/005/Question.jsx
+++ src/components/005/Question.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import img1 from '../../images/q-1.jpg'
 import { value } from '../../assets/value';
+import { question } from '../../assets/data';
 import { DotsThreeOutline } from 'phosphor-react';
 
-const Question = ()=>{
+const Question = ({idx,type,img,quiz})=>{
     return(
         <div className='text-xl font-semibold space-y-4'>
             <div className='flex items-center justify-between my-4'>
-                <p>1/10</p>
-                <p>Qiuz</p>
+                <p>{`${idx}/10`}</p>
+                <p>{type}</p>
                 <DotsThreeOutline className=' border-2 border-black rounded-full p-1' size={value.md} color="#000000" weight="bold" />
             </div>
-            <img className='w-full rounded-2xl' src={img1} alt="description" />
-            <p className='text-center w-3/4 mx-auto'>.... do you get to school? By bus?</p>
+            <img className='w-full rounded-2xl' src={img} alt="description" />
+            <p className='text-center w-3/4 mx-auto'>{quiz}</p>
         </div>
     )
 }
