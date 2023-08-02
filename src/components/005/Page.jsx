0182d5ab45ba86638b3f1be1779a0ed30b14667d
collins <kevin1520smart@gmail.com>
2023-08-02 21:59:29 +0100

    created Page

---
+++ src/components/005/Page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Factor from './Factor';
+import Question from './Question';
+
+
+const Page = ()=>{
+    return(
+        <div>
+            <Factor/>
+            <Question/>
+        </div>
+    )
+}
+
+
+export default Page; 
