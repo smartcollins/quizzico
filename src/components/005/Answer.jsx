ff1711fa1603b59ce5c03217511fd7b979f9186c
collins <kevin1520smart@gmail.com>
2023-08-06 11:24:10 +0100

    work in progress

--- src/components/005/Answer.jsx
+++ src/components/005/Answer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 
-const Answer = ({ans})=>{
+const Answer = ({idx,ans})=>{
+    const case1 = ''
     return(
-        <div>
-            {ans.map((item,idx)=><p key={idx}>{item}</p>)}
+        <div className={idx===1?'bg-red-300 grid grid-cols-2 gap-4 text-center items-center justify-center capitalize text-white h-[30vh]':'bg-red-500'}>
+            {ans.map((item,id)=><div className='bg-red-400 h-full [&:nth-child(2):bg-blue-500] first:text-blue-500' key={id}>
+                {idx===1&&<p className=''>{item}</p>}
+            </div>)}
         </div>
     )
 }
