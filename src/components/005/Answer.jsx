5b79a1e84eeec36568001a8b11d32332cec6b125
collins <kevin1520smart@gmail.com>
2023-08-06 16:17:24 +0100

    made it round

--- src/components/005/Answer.jsx
+++ src/components/005/Answer.jsx
@@ -6,7 +6,7 @@ const Answer = ({idx,ans})=>{
     // console.log(ans.length)
     return(
         <div className={ans.length >= 2 ?case1:'bg-red-500'}>
-            {ans.map((item,id)=><div className='h-full w-full bg-orange-500 relative first:rounder first:bg-blue-500 [&:nth-child(2)]:bg-red-500 [&:nth-child(3)]:bg-yellow-500 last:bg-green-500' key={id}>
+            {ans.map((item,id)=><div className='h-full w-full bg-orange-500 relative first:rounded-xl first:bg-blue-500 [&:nth-child(2)]:rounded-xl [&:nth-child(2)]:bg-red-500 [&:nth-child(3)]:rounded-xl [&:nth-child(3)]:bg-yellow-500 last:bg-green-500 last:rounded-xl' key={id}>
                 <div className='w-full h-full relative'>
                     <p className='absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2 w-2/4 text-center bg-red-500'>{item}</p>
                 </div>
