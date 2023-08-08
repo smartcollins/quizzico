8c29af7ed1964a523d3642b972424467ad5ba18f
collins <kevin1520smart@gmail.com>
2023-08-08 22:04:08 +0100

    formatted files

--- src/components/005/Question.jsx
+++ src/components/005/Question.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import img1 from '../../images/q-1.jpg'
-import { value } from '../../assets/value';
-import { question } from '../../assets/data';
-import { DotsThreeOutline } from 'phosphor-react';
+import React from "react";
+import { value } from "../../assets/value";
+import { DotsThreeOutline } from "phosphor-react";
 
-const Question = ({idx,type,img,quiz})=>{
-    return(
-        <div className='text-xl font-semibold space-y-4'>
-            <div className='flex items-center justify-between my-4'>
-                <p>{`${idx}/10`}</p>
-                <p>{type}</p>
-                <DotsThreeOutline className=' border-2 border-black rounded-full p-1' size={value.md} color="#000000" weight="bold" />
-            </div>
-            <img className='w-full rounded-2xl' src={img} alt="description" />
-            <p className='text-center w-3/4 mx-auto'>{quiz}</p>
-        </div>
-    )
-}
+const Question = ({ idx, type, img, quiz }) => {
+  return (
+    <div className="text-xl font-semibold space-y-4">
+      <div className="flex items-center justify-between my-4">
+        <p>{`${idx}/10`}</p>
+        <p>{type}</p>
+        <DotsThreeOutline className=" border-2 border-black rounded-full p-1" size={value.md} color="#000000" weight="bold"/>
+      </div>
+      <img className="w-full rounded-2xl" src={img} alt="description" />
+      <p className="text-center w-3/4 mx-auto">{quiz}</p>
+    </div>
+  );
+};
 
-
-export default Question; 
+export default Question;
